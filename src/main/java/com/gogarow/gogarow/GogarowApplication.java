3106adf5429877492f5437e81658ad4fbd0a590f
c4meronMcc <cameronmccreadiee@gmail.com>
2025-12-19 17:14:49 +0000

    inital commit

---
+++ src/main/java/com/gogarow/gogarow/GogarowApplication.java
@@ -0,0 +1,13 @@
+package com.gogarow.gogarow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogarowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogarowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gogarow/gogarow/GogarowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogarow.gogarow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogarowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
